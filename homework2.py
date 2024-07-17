0d5a52225d8d5efa1fa88ad610f1662f39b11567
KvalIvan <serga.vanya14@gmail.com>
2024-07-17 18:28:26 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Completed_tasks = 12
+time = 1.5
+Course_of_Study = 'Python'
+time2 = time / Completed_tasks
+print(f'Курс: {Course_of_Study}, всего задач: {Completed_tasks}, Затрачено часов: {time}, среднее время выполнения {time2} часа.')
